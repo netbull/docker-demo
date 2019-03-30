0463a12b405720fedabfd80760a1a2fed5c0a923
Krasmir Stavrev <stavrev.krasimir@gmail.com>
2019-03-30 12:45:51 +0200

    Polish create form

--- src/app/create-task/create-task.component.ts
+++ src/app/create-task/create-task.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-task',
   templateUrl: './create-task.component.html',
-  styleUrls: ['./create-task.component.sass']
+  styleUrls: ['./create-task.component.scss']
 })
 export class CreateTaskComponent {
   title: string;
